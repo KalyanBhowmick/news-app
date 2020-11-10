1a80d3dffb6db3f8fc4bc80cd8cde09230f01c32
Kalyan Bhowmick <kbhowmick067@gmail.com>
2020-11-10 11:17:26 +0530

    removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HeadlineCards from './components/HeadlineCards';
-import HeadlineDetails from './components/HeadlineDetails';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HeadlineCards >
-        <HeadlineDetails />
-      </HeadlineCards>
+      <HeadlineCards />
     </div>
   );
 }
